99c1d31af01bc862aad7a59e88d7cff9e1758c33
Marcus Broman <mbroman23@gmail.com>
2021-03-19 13:46:58 +0100

    Added RestSharp

---
+++ Source/SpacePark.App/Classes/SwStarship.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePark.App.Classes
+{
+    public class SwStarship
+    {
+        public int StarshipID { get; set; }
+        public string Name { get; set; }
+        public int LengthInM { get; set; }
+        public bool BrandIsVolvo { get; set; }  
+
+        public SwStarship()
+        {
+
+        }
+    }
+}
