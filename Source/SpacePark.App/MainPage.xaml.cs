7ecdb906738fcc479bc4427fb119de43a5c34749
fabbemannen <fabbemannen@gmail.com>
2021-03-23 16:30:38 +0100

    Revert "Trying to fix database and EF"
    This reverts commit c4aeab5387964a966c9ef6c9b263cb2a49c78e79.

--- Source/SpacePark.App/MainPage.xaml.cs
+++ Source/SpacePark.App/MainPage.xaml.cs
@@ -15,11 +15,10 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using RestSharp;
-using SpacePark.Classes;
+using SpacePark.App.Classes;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using SpacePark;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -32,7 +31,6 @@ namespace SpacePark.App
 
     public sealed partial class MainPage : Page
     {
-        
         public string Example = "Hello";
         public List<User> Users;
         public ObservableCollection<SwStarship> StarShips = new ObservableCollection<SwStarship>();
